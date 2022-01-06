213a6b0cb310cc373471d09a46086a26d7be1352
thteixeirap <thteixeirap@gmail.com>
2022-01-06 08:45:27 -0300

    Página Sobre

--- src/containers/sobre/index.js
+++ src/containers/sobre/index.js
@@ -6,11 +6,130 @@ import './index.css';
 
 
 function Sobre () {
+
+    window.addEventListener('scroll',reveal);
+
+    function reveal(){
+        var reveals = document.querySelectorAll('.reveal');
+
+        for(var i=0;i < reveals.length; i++){
+
+            var windowheight = window.innerHeight;
+            var revealtop = reveals[i].getBoundingClientRect().top;
+            var revealpoint = 150;
+
+            if(revealtop < windowheight - revealpoint){
+                reveals[i].classList.add('active');
+            }
+            else{
+                reveals[i].classList.remove('active');
+            }
+        }
+    }
     return(
     <>
-        <NavBar titulo="Sobre" />
+        <NavBar titulo= "Sobre" />
         <Container fluid>
-            <p className="text-white">Em construção...</p>
+            <div className='body-about'>
+                <section>       
+                    <div class="animated-title">
+                        <div class="text-top">
+                        <div>
+                            <span>HEP</span>
+                            <span>The Card Game</span>
+                        </div>
+                        </div>          
+                        <div class="text-bottom">
+                            <div>Sobre nós</div>
+                        </div>
+                    </div>
+            </section>
+            <section>
+                <div className='container reveal'>                
+                    <h2>Titulo</h2>
+                    <div className='cards'>
+                        <div className='text-card'>
+                            <h3>Titile</h3>
+                            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e 
+                                vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos 
+                                e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como 
+                                também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. 
+                                Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens 
+                                de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração 
+                                eletrônica como Aldus PageMakerLorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e 
+                                vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos 
+                                e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como 
+                                também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. 
+                                Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens 
+                                de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração 
+                                eletrônica como Aldus PageMaker..
+                            </p>
+                        </div>     
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div className='container reveal'>
+                    <h2>Titulo</h2>
+                    <div className='cards'>
+                        <div className='text-card'>
+                            <h3>Titile</h3>
+                            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e 
+                                vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos 
+                                e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos.
+                            </p>
+                        </div>
+                        <div className='text-card'>
+                            <h3>Titile</h3>
+                            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e 
+                                vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos 
+                                e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos.
+                            </p>
+                        </div>
+
+                        <div className='text-card'>
+                            <h3>Titile</h3>
+                            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e 
+                                vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos 
+                                e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <section>
+                <div className='container reveal'>
+                    <h2>Titulo</h2>
+                    <div className='cards'>
+                        <div className='text-card'>
+                            <h3>Titile</h3>
+                            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e 
+                                vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos 
+                                e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos.
+                            </p>
+                        </div>
+                        <div className='text-card'>
+                            <h3>Titile</h3>
+                            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e 
+                                vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos 
+                                e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos.
+                            </p>
+                        </div>
+
+                        <div className='text-card'>
+                            <h3>Titile</h3>
+                            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e 
+                                vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos 
+                                e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        
+        </div>
         </Container>
         <Footer />
     </>
